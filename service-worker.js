08bf47419689bfe1d8ce5740f068351fa6a0b027
adamforgety <adam.forgety@gmail.com>
2025-10-03 05:31:11 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('guitarviz-pwa-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+        // Add other assets here (CSS, JS, etc.)
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
